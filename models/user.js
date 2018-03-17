ecfaa2c89868e4b8d5f193268e119ff100bde5de
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MacBook-Pro.local>
2018-03-17 13:31:34 +0400

    Add validation

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ const mongoose              = require("mongoose"),
       passportLocalMongoose = require("passport-local-mongoose");
 
 let userSchema = new mongoose.Schema({
-    username: { type: String, /*unique: true, required: true*/ },
-    password: {type: String, /*required: true*/},
+    username: { type: String, unique: true, required: true },
+    password: {type: String, required: true},
     avatar: { type: String, default: "https://goo.gl/FHhKVq"},
     firstName: String,
     lastName: String,
-    email: {type: String, /*unique: true,*/ required: true},
+    email: {type: String, unique: true, required: true},
     resetPasswordToken: String,
     resetPasswordExpires: Date,
     isAdmin: {type: Boolean, default: false}
