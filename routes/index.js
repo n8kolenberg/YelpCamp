e08a62baeb55a7bc3833ea49917d1667e03257c2
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MacBook-Pro.local>
2018-03-26 21:38:51 +0400

    Refactor registration route to allow user to upload avatar image while still using express-validator

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,49 @@ const express       = require("express"),
       async         = require("async"),
       crypto        = require("crypto"), //This one comes with Express so you don't have to install it
       //The following is data for sending reset password emails through mailgun
-      api_key       = "key-8d2a840eb1b275bb2ec15ec6280dd48b",
-      domain        = "sandbox4a8684bd307f48fbbd154e2f2da15372.mailgun.org",
+      api_key       = process.env.mailgun_api_key,
+      domain        = process.env.mailgun_domain,
+      //Image upload
+      multer        = require("multer"),
       mailgun       = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
       //Express-validation http://tinyurl.com/ybpgyt76
 const { check, validationResult, body } = require('express-validator/check');
 const { matchedData, sanitize, sanitizeBody } = require('express-validator/filter');
+
+
+
+
+/* ======================================
+   IMAGE UPLOAD CONFIG
+   ====================================== */
+//Define a name for the image file you will upload that will consist of the date + original file name
+var storage = multer.diskStorage({
+    filename: function(req, file, callback) {
+        callback(null, Date.now() + file.originalname);
+    }
+});
+
+//Any image that gets uploaded must have an extension of jpg, jpeg, png or gif
+var imageFilter = function(req, file, cb) {
+    //accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error("Only image files are allowed!"), false);
+    }
+    cb(null, true);
+};
+
+//Uploading to cloudinary
+var upload = multer({storage: storage, fileFilter: imageFilter});
+
+var cloudinary = require("cloudinary");
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+/* ======================================
+   END IMAGE UPLOAD CONFIG */
 
 
 
@@ -72,29 +108,44 @@ router.post("/register", /*Validation middleware*/[
             if(err !== undefined) {
                 errorMsg.push(err.msg);
             }
-        });  
+        });
+        eval(require("locus"));
         
         //And then we flash them  
         req.flash("error", errorMsg);
         return res.redirect("back");
     }
     
-    // matchedData returns only the subset of data validated by the middleware
-    let newUser = matchedData(req);
-    newUser.firstName = req.body.firstName;
-    newUser.lastName = req.body.lastName;
-
-    User.register(newUser, req.body.password, (err, user) => {
-        if (err) {
-            req.flash("error", err.message);
-            return res.redirect("/register");
+    //Image upload
+    cloudinary.v2.uploader.upload(req.file.path, (err, result) => {
+        if(err) {
+            req.flash('error', err.message);
+            return res.redirect("back");
         }
-        passport.authenticate("local")(req, res, () => {
-            req.flash("success", `Welcome to YelpCamp, ${user.username}!` );
-            res.redirect("/campgrounds");
-        });
-    });
-});
+
+        //add cloudinary url for the image to the user object as image property
+        req.body.user.image = result.secure_url;
+        //add image's public_id to user object
+        req.body.user.image_id = result.public_id;
+        
+        // matchedData returns only the subset of data validated by the middleware
+        let newUser = matchedData(req);
+        newUser.firstName = req.body.firstName;
+        newUser.lastName = req.body.lastName;
+        
+
+        User.register(newUser, req.body.password, (err, user) => {
+            if (err) {
+                req.flash("error", err.message);
+                return res.redirect("/register");
+            }
+            passport.authenticate("local")(req, res, () => {
+                req.flash("success", `Welcome to YelpCamp, ${user.username}!`);
+                res.redirect("/campgrounds");
+            }); //End passport.authenticate()
+        }); //End User.register
+    }); //End cloudinary.uploader.upload()
+}) //End post route;
 
 /** SHOW THE LOGIN FORM ======= */
 router.get("/login", (req, res) => {
@@ -121,7 +172,6 @@ router.get("/logout", (req, res) => {
 
 /** SHOW THE USER PROFILE ======= */
 router.get("/users/:id", (req, res) => {
-    
     User.findById(req.params.id, (err, foundUser) => {
         if(err || !foundUser) {
             req.flash("error", "Woops! Unfortunately, that user doesn't seem to exist anymore");
@@ -176,16 +226,6 @@ router.post("/forgot", (req, res, next)=>{
         },
         /*Third functtion*/
         (token, user, done) => {
-            /**Using Nodemailer with Mailtrap to test reset password email */
-            // let smtpTransport = nodemailer.createTransport({
-            //     host: "smtp.mailtrap.io",
-            //     port: 2525,
-            //     auth: {
-            //         user: "3c081cbac4d8bf",
-            //         pass: "c8731cc1644a00"
-            //     }
-            // });
-
             let maildata = {
                 from: 'N8 at YelpCamp <nkolenberg@gmail.com>',
                 to: user.email,
@@ -199,18 +239,6 @@ http://${req.headers.host}/reset/${token}
 If you did not request this, please ignore this email and your password will remain unchanged 
                 `
             };
-
-            /**Using Nodemailer with Mailtrap to test reset password email */
-            // smtpTransport.sendMail(maildata, (err) => {
-            //     if(err) {
-            //         console.log("Error sending the email");
-            //         req.flash("error", "Error sending the email. Please try again.");
-            //         req.redirect("back");
-            //     }
-            //     console.log("mail sent");
-            //     req.flash("success", `An email has been sent to ${user.email} with further instructions`);
-            //     done(err, "done");    
-            // });
 
 
          /**Using Mailgun to test reset password email */
